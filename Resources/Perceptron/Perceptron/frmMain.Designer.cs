f038fb1c6b988dc57e29606782a059ec4ccaf65c
Vincenzo Marconi <vincenzorm117@gmail.com>
2024-06-29 15:31:54 -0600

    backed up perceptron resources

---
+++ Resources/Perceptron/Perceptron/frmMain.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace Perceptron
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnLearn = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.trackLearningRate = new System.Windows.Forms.TrackBar();
+            this.lblLearningRate = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtIterations = new System.Windows.Forms.TextBox();
+            this.pnlCanvas = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.trackLearningRate)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnLearn
+            // 
+            this.btnLearn.Location = new System.Drawing.Point(218, 160);
+            this.btnLearn.Name = "btnLearn";
+            this.btnLearn.Size = new System.Drawing.Size(142, 23);
+            this.btnLearn.TabIndex = 1;
+            this.btnLearn.Text = "Learn";
+            this.btnLearn.UseVisualStyleBackColor = true;
+            this.btnLearn.Click += new System.EventHandler(this.btnLearn_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(218, 189);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(142, 23);
+            this.btnClear.TabIndex = 2;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // trackLearningRate
+            // 
+            this.trackLearningRate.Location = new System.Drawing.Point(218, 46);
+            this.trackLearningRate.Maximum = 500;
+            this.trackLearningRate.Name = "trackLearningRate";
+            this.trackLearningRate.Size = new System.Drawing.Size(141, 45);
+            this.trackLearningRate.TabIndex = 3;
+            this.trackLearningRate.TickFrequency = 20;
+            this.trackLearningRate.Scroll += new System.EventHandler(this.trackLearningRate_Scroll);
+            // 
+            // lblLearningRate
+            // 
+            this.lblLearningRate.Location = new System.Drawing.Point(218, 20);
+            this.lblLearningRate.Name = "lblLearningRate";
+            this.lblLearningRate.Size = new System.Drawing.Size(142, 23);
+            this.lblLearningRate.TabIndex = 4;
+            this.lblLearningRate.Text = "Learning Rate: 0";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(218, 88);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(105, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Number of Iterations:";
+            // 
+            // txtIterations
+            // 
+            this.txtIterations.Location = new System.Drawing.Point(318, 85);
+            this.txtIterations.Name = "txtIterations";
+            this.txtIterations.Size = new System.Drawing.Size(42, 20);
+            this.txtIterations.TabIndex = 6;
+            this.txtIterations.Text = "100";
+            // 
+            // pnlCanvas
+            // 
+            this.pnlCanvas.BackColor = System.Drawing.Color.White;
+            this.pnlCanvas.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pnlCanvas.Location = new System.Drawing.Point(12, 12);
+            this.pnlCanvas.Name = "pnlCanvas";
+            this.pnlCanvas.Size = new System.Drawing.Size(200, 200);
+            this.pnlCanvas.TabIndex = 7;
+            this.pnlCanvas.Paint += new System.Windows.Forms.PaintEventHandler(this.pnlCanvas_Paint);
+            this.pnlCanvas.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pnlCanvas_MouseDown);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(372, 231);
+            this.Controls.Add(this.pnlCanvas);
+            this.Controls.Add(this.txtIterations);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblLearningRate);
+            this.Controls.Add(this.trackLearningRate);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnLearn);
+            this.Name = "frmMain";
+            this.Text = "Perceptron Simulation";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.trackLearningRate)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnLearn;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.TrackBar trackLearningRate;
+        private System.Windows.Forms.Label lblLearningRate;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtIterations;
+        private System.Windows.Forms.Panel pnlCanvas;
+    }
+}
+
---
+++ Resources/Perceptron/Perceptron/frmMain.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Perceptron
+{
+    public partial class frmMain : Form
+    {
+        List<Sample> samples = new List<Sample>();
+        Graphics objGraphics;
+        Graphics grph;
+        double w1, w2, w0 = 0;
+        double alpha = 0.5;
+        double x0 = -1;
+
+        int maxIterations;
+
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void picCanvas_MouseDown(object sender, MouseEventArgs e)
+        {
+            
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            objGraphics = pnlCanvas.CreateGraphics();
+            grph = pnlCanvas.CreateGraphics();
+
+            
+        }
+
+        private void btnLearn_Click(object sender, EventArgs e)
+        {
+            int i;
+            int iterations = 0;
+            bool error = true;
+
+            maxIterations = int.Parse(txtIterations.Text);
+
+            Random rnd = new Random();
+
+            w0 = rnd.NextDouble();
+            w1 = rnd.NextDouble();
+            w2 = rnd.NextDouble();
+
+            alpha = (double)trackLearningRate.Value / 1000;
+
+            while (error && iterations < maxIterations)
+            {
+                error = false;
+
+                for (i = 0; i <= samples.Count - 1; i++)
+                {
+                    double x1 = samples[i].X1;
+                    double x2 = samples[i].X2;
+                    int y;
+
+                    if (((w1 * x1) + (w2 * x2) - w0) < 0)
+                    {
+                        y = -1;
+                    }
+                    else
+                    {
+                        y = 1;
+                    }
+
+                    if (y != samples[i].Class)
+                    {
+                        error = true;
+
+                        w0 = w0 + alpha * (samples[i].Class - y) * x0 / 2; //ak by x0 bolo +1, tak nedelime 2
+                        w1 = w1 + alpha * (samples[i].Class - y) * x1 / 2;
+                        w2 = w2 + alpha * (samples[i].Class - y) * x2 / 2;
+                    }
+                }
+                objGraphics.Clear(Color.White);
+                DrawSeparationLine();
+                iterations++;
+            }
+        }
+
+        private void picCanvas_MouseMove(object sender, MouseEventArgs e)
+        {
+
+            double posX = (double)(e.X - pnlCanvas.Width / 2) / 10;
+            double posY = (double)(pnlCanvas.Height / 2 - e.Y) / 10;
+
+            this.Text = posX + " " + posY;
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            samples.Clear();
+            objGraphics.Clear(Color.White);
+            objGraphics.DrawLine(new Pen(Color.Gainsboro), new Point(0, pnlCanvas.Height / 2), new Point(pnlCanvas.Width, pnlCanvas.Height / 2));
+            objGraphics.DrawLine(new Pen(Color.Gainsboro), new Point(pnlCanvas.Width / 2, 0), new Point(pnlCanvas.Width / 2, pnlCanvas.Height));
+        }
+
+        private void DrawSamples()
+        {
+            foreach (Sample sample in samples)
+            {
+
+                double posX = (pnlCanvas.Width / 2) + sample.X1 * 10;
+                double posY = (pnlCanvas.Height / 2) - sample.X2 * 10;
+
+                Pen pen;
+                if (sample.Class == 1)
+                {
+                    pen = new Pen(Color.Blue);
+                }
+                else
+                {
+                    pen = new Pen(Color.Red);
+                }
+
+                objGraphics.DrawLine(pen, new Point((int)posX - 3, (int)posY), new Point((int)posX + 3, (int)posY));
+                objGraphics.DrawLine(pen, new Point((int)posX, (int)posY - 3), new Point((int)posX, (int)posY + 3));
+            }
+        }
+
+        private void DrawSeparationLine()
+        {
+            objGraphics.DrawLine(new Pen(Color.Gainsboro), new Point(0, pnlCanvas.Height / 2), new Point(pnlCanvas.Width, pnlCanvas.Height / 2));
+            objGraphics.DrawLine(new Pen(Color.Gainsboro), new Point(pnlCanvas.Width / 2, 0), new Point(pnlCanvas.Width / 2, pnlCanvas.Height));
+
+            Pen pen = new Pen(Color.DarkGreen);
+            double x1;
+
+            x1 = -10;
+            double y = -(x1 * w1 / w2) - ((x0*w0) / w2);
+
+            double shift = pnlCanvas.Height / 2;
+
+
+            Point p1 = new Point(0, (int)(shift - y * 10));
+
+            x1 = 10;
+            double y2 = -(x1 * w1 / w2) + ((w0) / w2);
+
+            Point p2 = new Point(pnlCanvas.Width, (int)(shift - y2 * 10));
+            if (w2 != 0)
+            {
+                objGraphics.DrawLine(pen, p1, p2);
+                DrawSamples();
+            }
+        }
+
+        private void trackLearningRate_Scroll(object sender, EventArgs e)
+        {
+            lblLearningRate.Text = "Learning Rate: " + (double)trackLearningRate.Value/1000;
+        }
+
+        private void pnlCanvas_MouseDown(object sender, MouseEventArgs e)
+        {
+            Sample sample;
+            Pen pen;
+
+            double posX = (double)(e.X - pnlCanvas.Width / 2) / 10;
+            double posY = (double)(pnlCanvas.Height / 2 - e.Y) / 10;
+
+            if (e.Button == MouseButtons.Left)
+            {
+                pen = new Pen(Color.Blue);
+                sample = new Sample(posX, posY, 1);
+            }
+            else
+            {
+                pen = new Pen(Color.Red);
+                sample = new Sample(posX, posY, -1);
+            }
+            samples.Add(sample);
+
+            objGraphics.DrawLine(pen, new Point(e.X - 3, e.Y), new Point(e.X + 3, e.Y));
+            objGraphics.DrawLine(pen, new Point(e.X, e.Y - 3), new Point(e.X, e.Y + 3));
+        }
+
+        private void pnlCanvas_Paint(object sender, PaintEventArgs e)
+        {
+            objGraphics.DrawLine(new Pen(Color.Gainsboro), new Point(0, pnlCanvas.Height / 2), new Point(pnlCanvas.Width, pnlCanvas.Height / 2));
+            objGraphics.DrawLine(new Pen(Color.Gainsboro), new Point(pnlCanvas.Width / 2, 0), new Point(pnlCanvas.Width / 2, pnlCanvas.Height));
+        }
+
+
+        
+    }
+}
